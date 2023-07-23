1c48cc7ecbe02666cb63257763285a23ef5ce8de
royaleddie <iginuaendurance@gmail.com>
2023-07-23 17:28:49 +0100

    New Update

--- base/views.py
+++ base/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
+from .models import Room
 
+
+# rooms = [
+#     {'id':1, 'name':'Hello'},
+#     {'id':2, 'name':'Lets learn Django'},
+#     {'id':3, 'name':'Its going to be fun'},
+# ]
 
 def home(request):
-    return render(request, 'home.html')
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    # room = None
+    # for i in rooms:
+    #     if i['id'] == int(pk):
+    #         room = i
+    
+    room = Room.objects.get(id=pk)
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
